15be825b31d021bec858338cf976523f93fb2e49
Brandon Fidelin  Ravomanana <135463036+branGitfox@users.noreply.github.com>
2024-07-06 22:09:06 +0300

    timeout before redirection

--- components/loader/Loader.js
+++ components/loader/Loader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image , StyleSheet} from 'react-native';
 import xxx from '../../assets/xxx.png'
 import Spinner from '../spinner/Spinner';
+
 const Loader = ({navigation}) => {
     return (
         <View style={styles.container}>
@@ -10,11 +11,12 @@ const Loader = ({navigation}) => {
         </View>
     )
 }
+
 const styles = StyleSheet.create({
     container:{
         width:'100%',
         height:1000,
-        backgroundColor:'green',
+        backgroundColor:'green', 
         flex:1,
         alignItems:'center',
         justifyContent:'center'
@@ -24,4 +26,5 @@ const styles = StyleSheet.create({
         height:300
     }
 })
+
 export default Loader;
